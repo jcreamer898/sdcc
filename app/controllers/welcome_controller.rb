c4161a781ddf73c3bcf201b579c5f40a6ddf45c8
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2014-07-24 13:12:50 -0500

    Working on streaming images

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,27 @@
 class WelcomeController < ApplicationController
+    before_action :init_twitter
+
+    def tweets
+        @tweets = Array.new
+        
+        @client.search("#sdcc AND photo", :result_type => "recent").take(20).each do |tweet|
+          @tweets.push tweet
+        end
+        
+        respond_to do |format|
+            format.html { render 'tweets' }
+            format.json { render json: @tweets }
+        end
+    end
+    
+    private
+
+    def init_twitter
+
+        @client = Twitter::REST::Client.new do |config|
+          config.consumer_key    = "RMIm7PREKRNaU3ZhutxNKlnvr"
+          config.consumer_secret = "JQNFlmN7ilCdIRqRGbC9I369PyNqzz1IdD2BHmOb2e54HbgyJ6"
+        end 
+    
+    end
 end
---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :username, :first, :last, :created_at, :updated_at
